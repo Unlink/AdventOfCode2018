e3aac41ebef0135a4f6caccda43efba48a6dbcbf
Michal Ďuračík <miso250593@gmail.com>
2018-12-21 12:36:44 +0100

    Day 20, still not working

--- 20/a.py
+++ 20/a.py
@@ -22,58 +22,105 @@ class Cell:
             return self.map.getCell(self.x, self.y+1) 
         if direction == 3:
             return self.map.getCell(self.x+1, self.y)
+    def goTo(self, direction):
+        neighbour = self.getNeighbour(direction)
+        self.doors[direction] = True
+        neighbour.doors[(direction + 2) % 4] = True
+        return neighbour
     
     
 class Map:
     def __init__(self):
-        self.beginCell = Cell(0, 0)
         self.cells = dict()
-        self.cells[(0,0)] = self.beginCell
         
-    
+    def getCell(self, x, y):
+        if not((x, y) in self.cells):
+            self.cells[(x,y)] = Cell(x,y,self)
+        return self.cells[(x,y)]
+        
+class Branhes:
+    def __init__(self, cell, restOfPath, steps):
+        self.cell = cell
+        self.branches = list()
+        self.steps = steps
+        #scan for branches
+        temp = ""
+        phatenties = 0
+        for c in restOfPath:
+            if c == '|' and phatenties == 0:
+                self.branches.append(temp)
+                temp = ""
+            elif c == '(':
+                phatenties += 1
+                temp += c
+            elif c == ')' and phatenties > 0:
+                phatenties -= 1
+                temp += c
+            elif c == ')' and phatenties == 0:
+                self.branches.append(temp)
+                temp = ""
+            else:
+                temp += c
+        for i in range(len(self.branches)):
+            self.branches[i] += temp
+            
+    def getBranch(self):
+        if self.hasMoreBranches():
+            return self.branches.pop()
+        else:   
+            return None
+    def hasMoreBranches(self):
+        return len(self.branches) > 0
+        
+dToI = {
+    'N': 0,
+    'W': 1,
+    'S': 2,
+    'E': 3
+}
+regex = "^$"
 with open("input.txt", "r") as file:
-    for line in file:
-        area.append(line.strip())
-        print(line.strip())
+    regex = file.read().strip()
         
-timer = 0
-while True:
-    newArea = [[c for c in row] for row in area]
+path = regex[1:-1]
+
+mapa = Map()
+room = mapa.getCell(0,0)
+branchStack = list()
+branchStack.append(Branhes(room, path+")", 0))
+steps = 0
+
+while len(branchStack) > 0:
+    branch = branchStack.pop()
+    if branch.hasMoreBranches():
+        branchStack.append(branch)
+        path = branch.getBranch()
+        room = branch.cell
+        for i in range(len(path)):
+            if path[i] == "(":
+                branchStack.append(Branhes(room, path[i+1:], 0))
+                break
+            else:
+                room = room.goTo(dToI[path[i]])
     
-    i = 0
-    for row in area:
-        j = 0
-        for cell in row:
-            surounding = iterateArea(area, j, i)
-            #print("["+str(i)+":"+str(j)+"] - "+str(surounding))
-            if cell == "." and countE(surounding, "|") >= 3:
-                newArea[i][j] = "|"
-            elif cell == "|" and countE(surounding, "#") >= 3:
-                newArea[i][j] = "#"
-            elif cell == "#":
-                if not(countE(surounding, "#") >= 1 and countE(surounding, "|") >= 1):
-                    newArea[i][j] = "."
-            j+=1
-        i+=1
-    area = newArea
-    
-    timer += 1
-    print("After" + str(timer))
-    
-    for row in area:
-        print("".join(row))
-    time.sleep(.1)
-    #print(chr(27) + "[2J")
-    
-    if timer == 10:
-        break;
         
-wood = 0
-lumberyards = 0
-for row in area:
-    wood += countE(row, "|")
-    lumberyards += countE(row, "#")
-    
-print("Wood: "+str(wood))
-print("Lumberyards: "+str(lumberyards))
-print(str(wood * lumberyards))
+        
+print((room.x, room.y, room.doors))
+
+minX = min([i[0] for i in mapa.cells.keys()])
+maxX = max([i[0] for i in mapa.cells.keys()])
+minY = min([i[1] for i in mapa.cells.keys()])
+maxY = max([i[1] for i in mapa.cells.keys()])
+
+print((minX, maxX, minY, maxY))
+
+for i in range(minY, maxY+1):
+    for j in range(minX, maxX+1):
+        cell = mapa.getCell(j,i)
+        print("#"+("-" if cell.doors[0] else "#"), end="")
+    print("#")
+    for j in range(minX, maxX+1):
+        cell = mapa.getCell(j,i)
+        print(("|" if cell.doors[1] else "#")+("X" if (i,j) == (0,0) else "."), end="")
+    print("#")
+print("#"*((maxX - minX + 1)*2+1))
